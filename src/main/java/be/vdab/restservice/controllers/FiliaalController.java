db551d1af4aeb95aea5d8d4a70c9671ca712651d
Arne <a.vaneycken@gmail.com>
2021-03-01 10:26:12 +0100

    12.15 Validatie

--- src/main/java/be/vdab/restservice/controllers/FiliaalController.java
+++ src/main/java/be/vdab/restservice/controllers/FiliaalController.java
@@ -5,7 +5,13 @@ import be.vdab.restservice.exceptions.FiliaalNietGevondenException;
 import be.vdab.restservice.services.FiliaalService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Arne Van Eycken
@@ -36,8 +42,15 @@ public class FiliaalController {
     }
 
     @PostMapping
-    void post(@RequestBody Filiaal filiaal){
+    void post(@RequestBody @Valid Filiaal filiaal){
         filiaalService.create(filiaal);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String,String> verkeerdeData(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
     }
 
 }
--- src/main/java/be/vdab/restservice/domain/Filiaal.java
+++ src/main/java/be/vdab/restservice/domain/Filiaal.java
@@ -1,6 +1,9 @@
 package be.vdab.restservice.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -19,8 +22,12 @@ public class Filiaal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String naam;
+    @NotBlank
     private String gemeente;
+    @NotNull
+    @PositiveOrZero
     private BigDecimal omzet;
 
     public Filiaal(String naam, String gemeente, BigDecimal omzet) {
